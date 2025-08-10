cd490215107a1ffc5498bd42f1ddab1ad82f6ee0
Bernard McWeeney <bernard.mcweeney2@mail.dcu.ie>
2025-08-10 20:10:30 +0100

    upgraded to tailwind css v4.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind(), 
     sitemap({
       changefreq: 'weekly',
       lastmod: new Date(),
     })
   ],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   site: 'https://greenberry.ie',
 });
